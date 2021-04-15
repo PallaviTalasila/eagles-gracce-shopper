1903b1944172a7a4b90b7c9fe6560dec71dd2e76
Pallavi <pallavi01261984@gmail.com>
2021-04-14 21:25:02 -0400

    Pushing DB change and git-ignore

--- db/index.js
+++ db/index.js
@@ -1,8 +1,17 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'grace-shopper'
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
-const client = new Client(DB_URL);
+// Connect to DB
+const { Client } = require("pg");
+const DB_NAME = "grace-shopper";
+//const DB_URL =   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
 
 // database methods
 
