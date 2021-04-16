9b924ee0a18a750d349f7b90074a992c2c2423ff
Pallavi <pallavi01261984@gmail.com>
2021-04-15 21:13:58 -0400

    DB Initial Data

--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
 // Connect to DB
-// Connect to DB
 const { Client } = require("pg");
 const DB_NAME = "grace-shopper";
 //const DB_URL =   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
@@ -13,11 +12,89 @@ const client = new Client({
       : undefined,
 });
 
-
 // database methods
+
+/**********************************Product Methods************************/
+
+async function createProduct({ title, description, price, quantity }) {
+  const query = `INSERT INTO
+      products(title, description,price,quantity)
+      VALUES($1, $2,$3,$4)
+      returning *`;
+  const values = [title, description, price, quantity];
+
+  try {
+    const {
+      rows: [product],
+    } = await client.query(query, values);
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**********************************User Methods************************/
+
+async function createUser({ username, password, email }) {
+  const query = `INSERT INTO
+      users(username, password,email)
+      VALUES($1, $2,$3)
+      returning *`;
+  const values = [username, password, email];
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(query, values);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**********************************Order Methods************************/
+
+async function createOrder({ userid, productid, price, quantity }) {
+  const query = `INSERT INTO
+      orders(userid, productid, price, quantity)
+      VALUES($1, $2,$3,$4)
+      returning *`;
+  const values = [userid, productid, price, quantity];
+
+  try {
+    const {
+      rows: [order],
+    } = await client.query(query, values);
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**********************************Review Methods************************/
+
+async function createReview({ userid, productid, reviewtext }) {
+  const query = `INSERT INTO
+      reviews(userid, productid, reviewtext)
+      VALUES($1, $2,$3)
+      returning *`;
+  const values = [userid, productid, reviewtext];
+
+  try {
+    const {
+      rows: [review],
+    } = await client.query(query, values);
+    return review;
+  } catch (error) {
+    throw error;
+  }
+}
 
 // export
 module.exports = {
   client,
-  // db methods
-}
+  createProduct,
+  createUser,
+  createOrder,
+  createReview,
+};
--- db/init_db.js
+++ db/init_db.js
@@ -1,7 +1,10 @@
 // code to build and initialize DB goes here1
 const {
   client,
-
+  createProduct,
+  createUser,
+  createOrder,
+  createReview,
 } = require("./index");
 
 async function buildTables() {
@@ -26,7 +29,8 @@ async function buildTables() {
       ID SERIAL PRIMARY KEY,
       TITLE TEXT UNIQUE NOT NULL,
       DESCRIPTION VARCHAR(255),
-      PRICE INTEGER , QUANTITY INTEGER);    
+      PRICE DECIMAL, 
+      QUANTITY INTEGER);    
     
     CREATE TABLE USERS (
       ID SERIAL PRIMARY KEY,
@@ -38,7 +42,8 @@ async function buildTables() {
       ID SERIAL PRIMARY KEY,
       USERID INTEGER REFERENCES USERS(ID),
       PRODUCTID INTEGER REFERENCES PRODUCTS(ID) NOT NULL,
-      PRICE integer, QUANTITY integer);    
+      PRICE decimal, 
+      QUANTITY integer);    
     
     CREATE TABLE REVIEWS (
         USERID INTEGER REFERENCES USERS(ID) NOT NULL,
@@ -56,6 +61,155 @@ async function buildTables() {
 async function populateInitialData() {
   try {
     // create useful starting data
+    console.log("Starting to create products...");
+
+    const productsToCreate = [
+      {
+        title: "Product1",
+        description: "product1 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product2",
+        description: "product2 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product3",
+        description: "product3 description",
+        price: 19.99,
+        quantity: 2,
+      },
+      {
+        title: "Product4",
+        description: "product4 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product5",
+        description: "product5 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product6",
+        description: "product6 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product7",
+        description: "product7 description",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        title: "Product8",
+        description: "product8 description",
+        price: 100,
+        quantity: 2,
+      },
+    ];
+
+    const products = await Promise.all(productsToCreate.map(createProduct));
+
+    console.log("Products created:");
+    console.log(products);
+    console.log("Finished creating products!");
+
+    console.log("Starting to create users...");
+
+    const usersToCreate = [
+      {
+        username: "user1",
+        password: "passcode1",
+        email: "user1@example.com",
+      },
+      {
+        username: "user2",
+        password: "passcode2",
+        email: "user2@example.com",
+      },
+      {
+        username: "user3",
+        password: "passcode3",
+        email: "user3@example.com",
+      },
+      {
+        username: "user4",
+        password: "passcode4",
+        email: "user4@example.com",
+      },
+      {
+        username: "user5",
+        password: "passcode5",
+        email: "user5@example.com",
+      },
+    ];
+
+    const users = await Promise.all(usersToCreate.map(createUser));
+
+    console.log("Users created:");
+    console.log(users);
+    console.log("Finished creating users!");
+
+    console.log("Starting to create orders...");
+
+    const ordersToCreate = [
+      {
+        userid: 1,
+        productid: 1,
+        price: 100,
+        quantity: 2,
+      },
+      {
+        userid: 2,
+        productid: 2,
+        price: 100,
+        quantity: 2,
+      },
+      {
+        userid: 3,
+        productid: 3,
+        price: 19.99,
+        quantity: 2,
+      },
+    ];
+
+    const orders = await Promise.all(ordersToCreate.map(createOrder));
+
+    console.log("Orders created:");
+    console.log(orders);
+    console.log("Finished creating orders!");
+
+    console.log("Starting to create reviews...");
+
+    const reviewsToCreate = [
+      {
+        userid: 1,
+        productid: 1,
+        reviewtext: "review text 1",
+      },
+      {
+        userid: 2,
+        productid: 2,
+        reviewtext: "review text 2",
+      },
+      {
+        userid: 3,
+        productid: 3,
+        reviewtext: "review text 3",
+      },
+    ];
+
+    const reviews = await Promise.all(reviewsToCreate.map(createReview));
+
+    console.log("Reviews created:");
+    console.log(reviews);
+    console.log("Finished creating reviews!");
   } catch (error) {
     throw error;
   }
