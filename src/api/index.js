ffec330d09d4e37014e834f2565283d5e1d17445
Pallavi <pallavi01261984@gmail.com>
2021-04-22 20:14:14 -0400

    Helper functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,127 @@
-import axios from 'axios';
+import axios from "axios";
+/***********************USER FUNCTIONS*******************/
+
+export async function register(username, password, email) {
+  try {
+    const payload = { username: username, password: password, email: email };
+
+    const { data } = await axios.post(`/api/register`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function login(username, password) {
+  try {
+    const payload = { username: username, password: password };
+
+    const { data } = await axios.post(`/api/login`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/***********************PRODUCTS******************************/
+
+export async function getAllProducts() {
+  try {
+    const { data } = await axios.get(`/api/products`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addProduct(title, description, price, quantity) {
+  try {
+    const payload = {
+      title: title,
+      description: description,
+      price: price,
+      quantity: quantity,
+    };
+    const { data } = await axios.post(`/api/products`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function editProduct(id, title, description, price, quantity) {
+  try {
+    const payload = {
+      title: title,
+      description: description,
+      price: price,
+      quantity: quantity,
+    };
+    const { data } = await axios.patch(`/api/products/${id}`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteProduct(id) {
+  try {
+    const { data } = await axios.delete(`/api/products/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/***********************ORDER/CART******************************/
+
+export async function getOrdersByUser(username) {
+  try {
+    const { data } = await axios.get(`/api/orders/${username}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addOrder(userid, productid, price, quantity) {
+  try {
+    const payload = {
+      userid: userid,
+      productid: productid,
+      price: price,
+      quantity: quantity,
+    };
+    const { data } = await axios.post(`/api/orders`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function editOrder(id, productid, quantity) {
+  try {
+    const payload = { productid: productid, quantity: quantity };
+    const { data } = await axios.patch(`/api/orders/${id}`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteOrder(id, productid) {
+  try {
+    const payload = { productid: productid };
+    const { data } = await axios.delete(`/api/orders/${id}`, payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export async function getSomething() {
   try {
-    const { data } = await axios.get('/api');
+    const { data } = await axios.get("/");
     return data;
   } catch (error) {
     throw error;
